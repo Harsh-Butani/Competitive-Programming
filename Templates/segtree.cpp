3a84799c1e0a4b9a0af00624bf203f86c4250b1a
Harsh Butani <70476451+Harsh-Butani@users.noreply.github.com>
2023-08-03 10:31:30 +0530

    Update segtree.cpp

--- Templates/segtree.cpp
+++ Templates/segtree.cpp
@@ -11,49 +11,59 @@ using ll = long long;
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr);
 #define FILE_IO freopen("file.in","r",stdin); freopen("file.out","w",stdout);
 
+struct item{
+
+};
+
 struct segtree{
-    ll sz;
-    vector<ll>values;
-    
-    ll merge(ll a,ll b){
-        return a+b;
+    int sz;
+    vector<item>values;
+
+    item NEUTRAL_ELEMENT=;
+
+    item merge(item a,item b){
+        // Merged value of item a and item b
+    }
+
+    item single(int v){
+        
     }
     
-    void init(ll n){
+    void init(int n){
         sz=1; 
         while(sz<n) sz<<=1; 
-        values.assign(2*sz,0LL);
+        values.resize(2*sz);
     }
     
-    void build(vector<ll>& a,ll x,ll lx,ll rx){
+    void build(vector<int>& a,int x,int lx,int rx){
         if(rx==lx+1){
-            if(lx<(ll)a.size()) values[x]=a[lx]; 
+            if(lx<(int)a.size()) values[x]=single(a[lx]); 
             return;
         } 
-        ll m=lx+(rx-lx)/2; 
+        int m=lx+(rx-lx)/2; 
         build(a,2*x+1,lx,m); 
         build(a,2*x+2,m,rx); 
         values[x]=merge(values[2*x+1],values[2*x+2]);
     }
     
-    void set(ll i,ll v,ll x,ll lx,ll rx){
+    void set(int i,int v,int x,int lx,int rx){
         if(rx==lx+1){
-            values[x]=v; 
+            values[x]=single(v); 
             return;
         } 
-        ll m=lx+(rx-lx)/2; 
+        int m=lx+(rx-lx)/2; 
         if(i<m) set(i,v,2*x+1,lx,m); 
         else set(i,v,2*x+2,m,rx); 
         values[x]=merge(values[2*x+1],values[2*x+2]);
     }
     
-    ll calc(ll l,ll r,ll x,ll lx,ll rx){
-        if(lx>=r || rx<=l) return 0; 
+    item calc(int l,int r,int x,int lx,int rx){
+        if(lx>=r || rx<=l) return NEUTRAL_ELEMENT; 
         if(lx>=l && rx<=r) return values[x]; 
-        ll m=lx+(rx-lx)/2; 
-        ll s1=calc(l,r,2*x+1,lx,m); 
-        ll s2=calc(l,r,2*x+2,m,rx); 
-        return merge(s1,s2);
+        int m=lx+(rx-lx)/2; 
+        item x1=calc(l,r,2*x+1,lx,m); 
+        item x2=calc(l,r,2*x+2,m,rx); 
+        return merge(x1,x2);
     }
 };
 
